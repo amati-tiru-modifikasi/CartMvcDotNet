89067901c46ff5847e7b6aa43c97ae49d01b385e
mdestafadilah <desta.08b@gmail.com>
2023-02-21 14:36:22 +0700

    first commit

---
+++ CartMvcDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CartMvcDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
